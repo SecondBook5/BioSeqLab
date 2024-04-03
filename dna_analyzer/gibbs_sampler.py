1d866374be753e7d50076bf06d2b39fe0303513f
SecondBook5 <ajbook12@gmail.com>
2024-04-03 03:38:57 -0400

    class gibbs sampler

--- dna_analyzer/gibbs_sampler.py
+++ dna_analyzer/gibbs_sampler.py
@@ -1,157 +1,159 @@
 import random
 
 
-def score(motifs):
-    '''Calculates the score of the given list of motifs.
+class GibbsSampler:
+    """
+    GibbsSampler is an iterative algorithm to find a collection of k-mers that
+    represent motifs in a set of DNA sequences. It randomly selects a k-mer
+    from each sequence and iteratively updates one k-mer at a time based on a
+    profile matrix, aiming to improve the motifs.
 
-    The score is calculated as the sum of differences between the most frequent nucleotide 
-    in each column of the motif matrix and the other nucleotides.
+    It starts with randomly chosen k-mers in each of t DNA sequences, and in
+    each iteration, it randomly selects an integer i between 1 and t and then
+    randomly changes only a single k-mer Motif. The process is repeated for N iterations.
+    """
 
-    Args:
-    - motifs (list): A list of motifs (strings).
+    def __init__(self, dna_list, k, t, N, repeats=20):
+        """
+        Initializes the GibbsSampler object with the given parameters.
 
-    Returns:
-    - score (int): The score of the motifs.
-    '''
-    # Create a list of strings, where each string represents a column of the motifs matrix
-    columns = [''.join(seq) for seq in zip(*motifs)]
-    # Calculate the maximum count of any nucleotide in each column
-    max_count = sum([max([c.count(nucleotide)
-                    for nucleotide in 'ACGT']) for c in columns])
-    # Calculate the score based on the sum of differences between the maximum count and the other counts
-    return len(motifs[0]) * len(motifs) - max_count
+        Args:
+        - dna_list (list): A list of DNA sequences.
+        - k (int): The length of the motifs to be searched for.
+        - t (int): The number of DNA sequences in the DNA list.
+        - N (int): The number of iterations.
+        - repeats (int): The number of times to repeat the algorithm to find the best motifs.
+        """
+        self.dna_list = dna_list
+        self.k = k
+        self.t = t
+        self.N = N
+        self.repeats = repeats
 
+    def gibbs_sampler(self):
+        """
+        Performs the Gibbs sampling algorithm.
 
-def profile(motifs):
-    '''Constructs the profile of the DNA list motifs.
+        Returns:
+        - best_motifs (list): The best collection of motifs found by the algorithm.
+        """
+        best_motifs_score = self.k * self.t
+        best_motifs = None
+        # Repeat the Gibbs sampling algorithm for a specified number of times
+        for _ in range(self.repeats):
+            bms, bm = self.gibbs_sampler_cycle()
+            # Update best motifs if a better set is found
+            if bms < best_motifs_score:
+                best_motifs = bm
+                best_motifs_score = bms
+        return best_motifs
 
-    The profile is a matrix where each column represents the probability of each nucleotide
-    at each position in the motifs.
+    def gibbs_sampler_cycle(self):
+        """
+        Performs one cycle of the Gibbs sampling algorithm.
 
-    Args:
-    - motifs (list): A list of motifs (strings).
+        Returns:
+        - best_motifs_score (int): The score of the best motifs found in this cycle.
+        - best_motifs (list): The best collection of motifs found in this cycle.
+        """
+        dna_length = len(self.dna_list[0])
+        # Initialize motifs with random k-mers from each DNA sequence
+        motifs = [self.dna_list[random.randint(
+            0, dna_length-self.k):random.randint(0, dna_length-self.k)+self.k] for _ in range(self.t)]
+        best_motifs = motifs[:]
+        best_motifs_score = self.score(best_motifs)
+        # Iterate through N iterations of updating motifs
+        for _ in range(self.N):
+            i = random.randint(0, self.t-1)
+            # Generate profile matrix from motifs excluding the i-th sequence
+            profile = self.profile_laplace(
+                [motif for index, motif in enumerate(motifs) if index != i])
+            # Update the i-th motif based on the profile
+            motifs[i] = self.profile_most_probable_kmer(
+                self.dna_list[i], profile)
+            # Calculate the score of the updated motifs
+            motifs_score = self.score(motifs)
+            # Update best motifs if the score improves
+            if motifs_score < best_motifs_score:
+                best_motifs_score = motifs_score
+                best_motifs = motifs[:]
+        return best_motifs_score, best_motifs
 
-    Returns:
-    - profile (list): The profile matrix.
-    '''
-    # Create a list of strings, where each string represents a column of the motifs matrix
-    columns = [''.join(seq) for seq in zip(*motifs)]
-    # Calculate the probability of each nucleotide at each position in the motifs
-    return [[float(col.count(nuc)) / float(len(col)) for nuc in 'ACGT'] for col in columns]
+    def score(self, motifs):
+        '''
+        Calculates the score of the given list of motifs.
 
+        The score is calculated based on the number of nucleotides that do not match the most common nucleotide
+        at each position across all motifs.
 
-def profile_most_probable_kmer(dna, k, profile):
-    '''Finds the profile most probable k-mer for the given input data.
+        Args:
+        - motifs (list): A list of motifs (k-mers).
 
-    Args:
-    - dna (str): A DNA sequence.
-    - k (int): The length of the k-mer.
-    - profile (list): The profile matrix.
+        Returns:
+        - score (int): The score of the motifs.
+        '''
+        # Count occurrences of each nucleotide in each column of the motifs
+        counts = [{nucleotide: column.count(
+            nucleotide) for nucleotide in 'ACGT'} for column in zip(*motifs)]
+        # Calculate the score based on the counts
+        return sum(len(motifs) - max(counts[i].values()) for i in range(len(counts)))
 
-    Returns:
-    - most_probable (str): The most probable k-mer in the sequence.
-    '''
-    # A dictionary relating nucleotides to their position within the profile.
-    nuc_loc = {nucleotide: index for index, nucleotide in enumerate('ACGT')}
+    def profile(self, motifs):
+        '''
+        Constructs the profile matrix of the DNA list motifs.
 
-    # Initialize the maximum probability.
-    max_probability = -1
+        The profile matrix represents the probability of each nucleotide at each position in the motifs.
 
-    # Compute the probability of each k-mer, store it if it's currently a maximum.
-    for i in range(len(dna)-k+1):
-        # Get the current probability.
-        current_probability = 1
-        for j, nucleotide in enumerate(dna[i:i+k]):
-            # Multiply the probability of each nucleotide at each position
-            current_probability *= profile[j][nuc_loc[nucleotide]]
+        Args:
+        - motifs (list): A list of motifs (k-mers).
 
-        # Check for a maximum.
-        if current_probability > max_probability:
-            max_probability = current_probability
-            most_probable = dna[i:i+k]
+        Returns:
+        - profile (list of lists): The profile matrix.
+        '''
+        # Count occurrences of each nucleotide in each column of the motifs and normalize
+        counts = [{nucleotide: column.count(
+            nucleotide) / len(column) for nucleotide in 'ACGT'} for column in zip(*motifs)]
+        return counts
 
-    return most_probable
+    def profile_most_probable_kmer(self, dna, profile):
+        '''
+        Finds the profile most probable k-mer for the given input data.
 
+        Args:
+        - dna (str): A DNA sequence.
+        - profile (list of lists): The profile matrix.
 
-def profile_laplace(motifs):
-    '''Constructs the Laplace profile of the DNA list motifs.
+        Returns:
+        - most_probable (str): The most probable k-mer in the sequence.
+        '''
+        max_probability = -1
+        most_probable = ''
+        # Iterate through all possible k-mers in the given DNA sequence
+        for i in range(len(dna) - self.k + 1):
+            kmer = dna[i:i+self.k]
+            probability = 1
+            # Calculate the probability of the k-mer using the profile
+            for j, nucleotide in enumerate(kmer):
+                probability *= profile[j][nucleotide]
+            # Update the most probable k-mer if the probability is higher
+            if probability > max_probability:
+                max_probability = probability
+                most_probable = kmer
+        return most_probable
 
-    The Laplace profile is a matrix where each column represents the probability of each nucleotide
-    at each position in the motifs with Laplace smoothing applied.
+    def profile_laplace(self, motifs):
+        '''
+        Constructs the Laplace profile matrix of the DNA list motifs.
 
-    Args:
-    - motifs (list): A list of motifs (strings).
+        Laplace smoothing is applied to avoid zero probabilities.
 
-    Returns:
-    - profile (list): The profile matrix.
-    '''
-    # Create a list of strings, where each string represents a column of the motifs matrix
-    columns = [''.join(seq) for seq in zip(*motifs)]
-    # Calculate the Laplace-smoothed probability of each nucleotide at each position in the motifs
-    return [[float(col.count(nuc) + 1) / float(len(col) + 4) for nuc in 'ACGT'] for col in columns]
+        Args:
+        - motifs (list): A list of motifs (k-mers).
 
-
-def _gibbs_sampler_cycle(dna_list, k, t, N):
-    '''Performs a single cycle of the Gibbs Sampler algorithm.
-
-    Args:
-    - dna_list (list): A list of DNA sequences.
-    - k (int): The length of the motifs to search for.
-    - t (int): The number of sequences in dna_list.
-    - N (int): The number of iterations to perform.
-
-    Returns:
-    - best_motifs_score (int): The score of the best motifs found.
-    - best_motifs (list): The best motifs found.
-    '''
-    # Calculate the length of the DNA sequences
-    dna_length = len(dna_list[0])
-    # Generate random start indices for the motifs in each sequence
-    random_integers = [random.randint(0, dna_length-k) for i in range(t)]
-    # Initialize motifs with random k-mers from each sequence
-    motifs = [dna_list[i][r:r+k] for i, r in enumerate(random_integers)]
-    # Initialize the best motifs and score with the initial motifs
-    best_motifs = list(motifs)
-    best_motifs_score = score(best_motifs)
-    # Perform N iterations of the Gibbs Sampler algorithm
-    for j in range(N):
-        # Randomly choose a sequence index to update its motif
-        i = random.randint(0, t-1)
-        # Construct a profile matrix from the other motifs
-        profile = profile_laplace(
-            [motif for index, motif in enumerate(motifs) if index != i])
-        # Update the motif for sequence i based on the profile
-        motifs[i] = profile_most_probable_kmer(dna_list[i], k, profile)
-        # Calculate the score of the updated motifs
-        motifs_score = score(motifs)
-        # Update the best motifs and score if the updated motifs have a lower score
-        if motifs_score < best_motifs_score:
-            best_motifs_score = motifs_score
-            best_motifs = motifs
-    return best_motifs_score, best_motifs
-
-
-def gibbs_sampler(dna_list, k, t, N):
-    '''Runs the Gibbs Sampler algorithm to find the best motifs.
-
-    Args:
-    - dna_list (list): A list of DNA sequences.
-    - k (int): The length of the motifs to search for.
-    - t (int): The number of sequences in dna_list.
-    - N (int): The number of iterations to perform.
-
-    Returns:
-    - best_motifs (list): The best motifs found.
-    '''
-    # Initialize the best motifs score with the maximum possible score
-    best_motifs_score = k * t
-    best_motifs = None
-    # Repeat the Gibbs Sampler algorithm multiple times to increase the chance of finding the best motifs
-    for repeat in range(30):
-        # Perform a single cycle of the Gibbs Sampler algorithm
-        bms, bm = _gibbs_sampler_cycle(dna_list, k, t, N)
-        # Update the best motifs and score if the current cycle produced a better result
-        if bms < best_motifs_score:
-            best_motifs = bm
-            best_motifs_score = bms
-    return best_motifs
+        Returns:
+        - profile (list of lists): The Laplace profile matrix.
+        '''
+        # Count occurrences of each nucleotide in each column of the motifs with Laplace smoothing
+        counts = [{nucleotide: (column.count(nucleotide) + 1) / (len(column) + 4)
+                   for nucleotide in 'ACGT'} for column in zip(*motifs)]
+        return counts
